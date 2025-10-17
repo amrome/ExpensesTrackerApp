3fbbf9d5827b1f6699d84632c6848977f12a52db
Amro Elian <elianamro5@gmail.com>
2025-10-17 23:11:06 +0300

    2

--- screens/AllExpenses.js
+++ screens/AllExpenses.js
@@ -2,6 +2,6 @@ import { Text } from "react-native";
 import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
 
 function AllExpenses() {
-  return <ExpensesOutput expensesPeriod="Total" />;
+  return <ExpensesOutput expensesPeriod="Totalllllllll" />;
 }
 export default AllExpenses;
